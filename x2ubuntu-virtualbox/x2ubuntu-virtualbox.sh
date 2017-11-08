3a10cb319358aef347175df9f701ae8561618c38
shimjye <shimjye@gmail.com>
2017-11-08 22:39:39 +0900

    shell port

--- x2ubuntu-virtualbox/x2ubuntu-virtualbox.sh
+++ x2ubuntu-virtualbox/x2ubuntu-virtualbox.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run -d -t --name x2vbox -v /data:/data -p 22:2223 shimjye/x2ubuntu-virtualbox
+docker run -d -t --name x2vbox -v /data:/data -p 2223:22 shimjye/x2ubuntu-virtualbox
 
