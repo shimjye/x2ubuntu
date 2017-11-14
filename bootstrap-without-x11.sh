8370ae77ee4d648e54e937bd36df0aaee99272f2
shimjye <shimjye@gmail.com>
2017-11-14 15:53:05 +0900

    data folder

--- bootstrap-without-x11.sh
+++ bootstrap-without-x11.sh
@@ -2,7 +2,7 @@
 
 # apt-get install
 sudo apt-get update
-sudo apt-get install -y ufw vim git terminator curl wget
+sudo apt-get install -y ufw vim git curl wget
 
 # docker setting
 curl -fsSL get.docker.com -o get-docker.sh
@@ -14,10 +14,3 @@ sudo systemctl enable docker
 # ufw setting
 sudo sed -i 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/g' /etc/default/ufw
 sudo ufw enable
-
-# data dir
-sudo mkdir /data && sudo chown ${USER}:${USER} /data
-sudo mkdir /data/x2ubuntu && sudo chown  ${USER}:${USER} /data/x2ubuntu
-sudo git clone https://github.com/shimjye/x2ubuntu /data/x2ubuntu
-
-sudo apt-get clean
