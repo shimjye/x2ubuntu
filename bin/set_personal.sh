66e6cfdf15534545bf023bd20980666854702a0f
Jae-Young Shim <jyshim.wk@gmail.com>
2018-04-14 17:08:39 +0900

    docker hub build done

--- bin/set_personal.sh
+++ bin/set_personal.sh
@@ -3,7 +3,8 @@ sudo apt-get install -y wget curl vim git terminator
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get install apt-transport-https
 sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt-get install -y google-chrome-stable
 
 exit 0
--- x2ubuntu-kr-tomcat/x2ubuntu-tomcat.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker run -d -t --name x2tomcat -v /data:/data -p 2224:22 -p 9980:8080 shimjye/x2ubuntu-tomcat
-
