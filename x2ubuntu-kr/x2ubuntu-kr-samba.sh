d12dd22b71d190c2b8032217e57468524a0b9fdf
Jae-Young Shim <jyshim.wk@gmail.com>
2018-05-03 11:00:35 +0900

    delete chrome-script

--- x2ubuntu-kr/x2ubuntu-kr-samba.sh
+++ x2ubuntu-kr/x2ubuntu-kr-samba.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker run -d -p 2223:22 -p 139:139 -p 445:445 --name x2samba -v /data:/data shimjye/x2ubuntu-kr
-
+# run x2ubuntu/bin/set_samba.sh
--- x2ubuntu-kr/x2ubuntu-kr-scm.sh
+++ x2ubuntu-kr/x2ubuntu-kr-scm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker run -d -p 2225:22 -p 9990:8080 --name x2scm -v /data:/data shimjye/x2ubuntu-kr
-
+# run x2ubuntu/bin/set_scm_manager.sh
