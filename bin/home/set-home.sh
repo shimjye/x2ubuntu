dc75648ffbb5fac3d88b7ed6c2220715133af8dd
Jae-Young Shim <jyshim.wk@gmail.com>
2018-06-02 10:41:22 +0900

    vim vundle

--- bin/home/set-home.sh
+++ bin/home/set-home.sh
@@ -9,6 +9,6 @@ cp ./bashrc ~/.bashrc
 cp ./vimrc ~/.vimrc
 cp ./user-dirs.dirs ~/.config/
 cp ./xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
-
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 exit 0
