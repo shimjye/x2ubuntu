020b8b06378cb8c3f26362b28655f693a85d2003
Jae-Young Shim <jyshim.wk@gmail.com>
2018-06-08 16:57:48 +0900

    samba

--- bin/set-samba.sh
+++ bin/set-samba.sh
@@ -2,4 +2,15 @@
 sudo apt-get install -y samba system-config-samba
 sudo touch /etc/libuser.conf
 
+sudo adduser --no-create-home --disabled-password --disabled-login jy
+sudo adduser --no-create-home --disabled-password --disabled-login jj
+sudo smbpasswd -a jy
+sudo smbpasswd -a jj
+sudo cp /data/x2ubuntu/bin/samba/* /etc/samba
+sudo chown root:root /etc/samba/*
+
+sudo mkdir /data/pub
+sudo mkdir /data/pub/jy
+sudo mkdir /data/pub/jj
+
 exit 0
